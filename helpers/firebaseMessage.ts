50a3aa1e70f4ba8865f899f93f46b07deb270ce6
Lucifer <luciferxhnh@gmail.com>
2025-05-09 20:48:54 +0700

    fix firebase config

--- helpers/firebaseMessage.ts
+++ helpers/firebaseMessage.ts
@@ -1,7 +1,21 @@
 const admin = require("firebase-admin");
 
 // Khởi tạo Firebase Admin SDK với service account key
-const serviceAccount = require("../firebase-config.json");
+// const serviceAccount = require("../firebase-config.json");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "jobs-employment-utem",
+  private_key_id: "56680d3b925f5b405790291751eed29d249a04d3",
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: "firebase-adminsdk-fbsvc@jobs-employment-utem.iam.gserviceaccount.com",
+  client_id: "104760828291472319741",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40jobs-employment-utem.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
