940f462d31d0ac2e692913adea31d63d6423cac3
Lucifer <luciferxhnh@gmail.com>
2025-05-21 20:15:45 +0700

    validate job category helper

--- api/v1/validates/admins/jobs.validate.ts
+++ api/v1/validates/admins/jobs.validate.ts
@@ -5,7 +5,8 @@ import {
   filterQueryLevelJobs,
   filterQueryWorkExperienceJobs,
 } from "../../../../helpers/filterQuery Validate";
-export const createRecord = (req: Request, res: Response, next: any): void => {
+import filterJobCategory from "../../../../helpers/filterJobCategory";
+export const createRecord = async (req: Request, res: Response, next: any): Promise<void> => {
   try {
     //Lấy dữ liệu người dùng gửi lên
     const title: string = req.body.title.toString();
@@ -41,6 +42,13 @@ export const createRecord = (req: Request, res: Response, next: any): void => {
 
     if (!job_categorie_id) {
       res.status(400).json({ error: "Vui Lòng Chọn Danh Mục Công Việc!" });
+      return;
+    }
+
+    const category = await filterJobCategory(job_categorie_id)
+
+    if (!category) {
+      res.status(400).json({ error: "Danh Mục Công Việc Không Hợp Lệ!" });
       return;
     }
 
