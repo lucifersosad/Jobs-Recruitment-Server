4038354055b202d902b9d5edcbdd432a595eebe2
Lucifer <luciferxhnh@gmail.com>
2025-06-02 22:03:27 +0700

    upgrade skill suggestion

--- api/v1/routes/common/index.routes.ts
+++ api/v1/routes/common/index.routes.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
 import { schoolRoutes } from "./school.routes";
+import { skillRoutes } from "./skills.routes";
 const routesCommonVersion1 = (app: Express) : void => {
     const version = "/api/v1/client";  
     app.use(version + "/schools", schoolRoutes);
+    app.use(version + "/skills", skillRoutes);
 }
 export default routesCommonVersion1
